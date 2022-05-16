a0e4c161791a347d1609782676cbb7e6adc10913
minnawat <70904343+minnawat@users.noreply.github.com>
2022-05-17 00:51:54 +0700

    Add files via upload

--- Project_4th_year/cvtest.py
+++ Project_4th_year/cvtest.py
@@ -40,7 +40,7 @@ class video():
 
 		print("start video")
 		# define a video capture object
-		vid = cv2.VideoCapture(0)
+		vid = cv2.VideoCapture(2)
 		# define pytesseract
 			
 			# Capture the video frame
